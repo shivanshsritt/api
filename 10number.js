842372fa179adf0ae4f87e246840e0185978423b
shivansh srivastava <catkelvis@gmail.com>
2024-07-02 13:22:06 +0530

    minior update

---
+++ 10number.js
@@ -0,0 +1,9 @@
+const marks = 46;  
+console.log(marks);
+
+// at this part you only  declare number 
+const  roll  = new Number(6117); 
+console.log(roll);
+
+console.log(roll.tofixed(4));
+
--- 2conversion.js
+++ 2conversion.js
@@ -8,5 +8,15 @@ console.log(typeof roll ,roll);
 
 
 
+let istrue ; 
+istrue= true
+console.log(istrue);
+
+console.log( Boolean(100))
 
 
+
+let num = 4;
+num = 0;
+console.log(Boolean(num)
+);
