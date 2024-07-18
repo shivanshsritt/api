c15682e6a5a5228d6bdceff60be85df8ef4effd3
shivansh srivastava <catkelvis@gmail.com>
2024-07-18 14:48:10 +0530

    update file

--- basic2/object/4object.js
+++ basic2/object/4object.js
@@ -1,9 +1,10 @@
 // array of object 
-const user  =[
-{
-name :"shivansh",
+const user  ={
+
+name :     "shivansh",
 id : 61117,
-}
+};
 
 
-]
+const {id : sonu } = user;
+console.log(id);
---
+++ basic3/function.js
@@ -0,0 +1,37 @@
+
+// declare a function 
+
+function myName ()  {
+
+console.log("s")
+console.log("h")
+console.log("i")
+console.log("v")
+console.log("a")
+console.log("n")
+console.log("s")
+console.log("h")
+
+}
+
+// function name  - myName  and (myName) is reference - ()  this is execution 
+
+// myName ()
+
+
+function sum (number1 ,number2)  //in this section it is paremeter
+{
+
+let result = number1+number2
+return result
+
+}
+
+
+const result = addtwonumber(3,5)
+
+
+sum(3,  5)      //and this part of code is  passing the argument of function 
+
+
+console.log(result);
