1b54946c1755aaf1766d899a86a2918f2dac895e
shivansh srivastava <catkelvis@gmail.com>
2024-08-02 22:50:17 +0530

    add event files

---
+++ event/sradha.js
@@ -0,0 +1,10 @@
+let buttons = document.querySelector('#first')
+
+buttons.onclick = ()=>{
+  console.log("button was click");
+  let age  =25
+  age++;
+  console.log(age);
+  
+}
+ element.addEventListener('mouseover', function() { console.log('Mouse over element!'); });
